752823d32a4d5c31c1d929aa8e35309f224f4b25
Denis Lunev <fjewfish@MacBook-Pro.local>
2020-09-26 14:59:19 +0300

    first commit

---
+++ v2.0/main_header_v2.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_HEADER_V2_H
+# define MAIN_HEADER_V2_H
+
+#include "libft/libft.h"
+#include "mlx/mlx.h"
+# include <math.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+# define NONE 0xFF000000
+# define WHITE 0x00FFFFFF
+# define BLACK 0x00000000
+# define RED 0x00FF0000
+# define GREEN 0x0000FF00
+# define BLUE 0x000000FF
+# define MAGENTA 0x00FF00FF
+# define YELLOW 0x00FFFF00
+# define CYAN 0x0000FFFF
+
+# define ESC 53
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define UP 126
+# define DOWN 125
+# define LEFT 123
+# define RIGHT 124
+
+# define SPEED 15
+# define TURN 0.05
+
+
+
+#endif

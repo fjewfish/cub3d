95dc8f8778f89633915dab9937ab9dd8b4add95e
Denis Lunew <fjewfish@mi-p5.msk.21-school.ru>
2020-09-26 18:34:33 +0300

    example

---
+++ v2.0/useful_fun_v2.c
@@ -0,0 +1,9 @@
+#include "main_header_v2.h"
+
+int		ft_spaceskip(char *line, int *i)
+{
+	while ((line[*i] == ' ' || line[*i] == '\t' || line[*i] == '\n')
+	|| (line[*i] == '\r' || line[*i] == '\v' || line[*i] == '\f'))
+		(*i)++;
+	return (1);
+}
